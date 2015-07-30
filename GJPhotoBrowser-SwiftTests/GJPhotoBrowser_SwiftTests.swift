45ccad59982d7355deb06a5514cca53a55e7ab80
imooc_gj <coder_wugaojun@163.com>
2015-07-30 14:13:33 +0800

    Initial Commit

---
+++ GJPhotoBrowser-SwiftTests/GJPhotoBrowser_SwiftTests.swift
@@ -0,0 +1,36 @@
+//
+//  GJPhotoBrowser_SwiftTests.swift
+//  GJPhotoBrowser-SwiftTests
+//
+//  Created by imooc_gj on 15/7/30.
+//  Copyright (c) 2015年 devgj. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class GJPhotoBrowser_SwiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
